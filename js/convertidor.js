a1c94d51d330cde7a80b4f174fdebc7c277c28ce
Derek Monge <83934852+DerekMarceloMongeAguilar@users.noreply.github.com>
2023-10-26 17:02:17 -0600

    Arreglando articulos

--- js/convertidor.js
+++ js/convertidor.js
@@ -1,7 +1,20 @@
 function Librasakg() {
-    const libras = parseFloat(document.getElementById("librasInput").value);
-    //Realizamos la conversión de libras a kilogramos ya que sabemos que 1 lb es igual a 0.45359237 kg
-    const kilogramos = libras * 0.45359237;
-    document.getElementById("resultadokilitogramos").textContent = kilogramos.toFixed(2) + " kg";
-}
 
+    const librasInput = document.getElementById("librasInput");
+
+    const resultadoKilogramos = document.getElementById("resultadokilitogramos");
+
+    const libras = parseFloat(librasInput.value);
+
+    if (isNaN(libras)) {
+        resultadoKilogramos.textContent = "Número inválido, ingrese un número válido de libras por favor.";
+    } else if (libras < 0) {
+        resultadoKilogramos.textContent = "No se permiten numeros negativos, ingrese un número válido de libras.";
+    } else {
+        /* 1 libra es igual a 0.45359237 kilogramos */
+        const kilogramos = libras * 0.45359237;
+
+        /* Se muestra el resultado */
+        resultadoKilogramos.textContent = kilogramos.toFixed(2) + " kg";
+    }
+}
